a004701bdf30c77e3028d8ff4cd98ffc59ece493
ericywong <ericwong26@gmail.com>
2018-01-26 14:36:19 -0500

    first commit, added comments, dynamic_texts and variables

---
+++ README.md
@@ -0,0 +1,3 @@
+# php-index
+
+This is a collection of everything I have learned/am learning about php. It is an index for future reference.
---
+++ variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+ <?php   
+
+    $name = "Eric";
+    $number = 23;
+    $Number_list = 100.5; //takes more memory
+    // $num-ber = 435;
+    // $0number = 500;
+    $NUMBER = 23; // all the number ones are different variables
+    $NUMBeR = "23"; // this is a string
+
+    // the "." is for contatenation
+    echo $name . " " . $NUMBeR;
+?>
+</body>
+</html>
